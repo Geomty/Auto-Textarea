71eb862b1d7c7c746c3ba44a6075ba39ec801b88
Geomty <69478549+Geomty@users.noreply.github.com>
2025-01-24 14:49:38 -0500

    checking textarea functionality

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 (async () => {
     let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     chrome.scripting.executeScript({
-        func: automator,
-        args: ["#00c5ff"],
+        func: checkTextarea,
         target: { tabId: tab.id }
+    }, ([results]) => {
+        if (results.result) {
+            document.getElementById("detected").innerHTML = "Textarea detected";
+        }
     })
 })();
 
-function automator(color) {
-    document.getElementsByTagName("body")[0].style.backgroundColor = color;
+function checkTextarea() {
+    return document.getElementsByTagName("textarea").length > 0;
 }
