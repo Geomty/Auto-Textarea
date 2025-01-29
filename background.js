8cefada8b2c7f7596a4d6d688e8b2eac1656a9a0
Geomty <69478549+Geomty@users.noreply.github.com>
2025-01-29 14:16:18 -0500

    finish functionality

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.runtime.onStartup.addListener(() => chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' }));
+let tab;
 
 chrome.storage.onChanged.addListener(changes => {
-    if (changes.tab.newValue) {
+    tab = changes.tab?.newValue || tab;
+    if (changes.times.newValue) {
         chrome.scripting.executeScript({
             files: ["./scripts/automator.js"],
-            target: { tabId: changes.tab.newValue }
+            target: { tabId: tab }
         });
     }
 });
--- scripts/automator.js
+++ scripts/automator.js
@@ -1,8 +1,18 @@
-var textarea = document.getElementsByTagName("textarea")[0];
-var submit = document.querySelectorAll("input[type=submit]")[0];
+try {
+    var textarea = document.getElementsByTagName("textarea")[0];
+    var submit = document.querySelectorAll("input[type=submit]")[0];
 
-chrome.storage.session.get(["character"]).then(({ character }) => {
-    textarea.value += character;
-    submit.click();
-    chrome.storage.session.clear();
-});
+    chrome.storage.session.get(["character", "interval", "times"]).then(items => {
+        textarea.value += items.character;
+        submit.click();
+        setTimeout(() => {
+            if (items.times > 0) {
+                chrome.storage.session.set({ times: items.times - 1 });
+            } else {
+                chrome.storage.session.clear();
+            }
+        }, items.interval * 1000);
+    });
+} catch (error) {
+    console.log(error);
+}
