19a204af60146cf0071cb41e365b8224a93bc41d
Geomty <69478549+Geomty@users.noreply.github.com>
2025-02-01 16:22:48 -0500

    bugfix

--- background.js
+++ background.js
@@ -1,13 +1,21 @@
 chrome.runtime.onStartup.addListener(() => chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' }));
-let tab;
+let tab, interval;
 
 chrome.storage.onChanged.addListener(changes => {
     tab = changes.tab?.newValue || tab;
+    interval = changes.interval?.newValue || interval;
+
     if (changes.times.newValue) {
-        chrome.scripting.executeScript({
-            files: ["./scripts/automator.js"],
-            target: { tabId: tab }
-        });
+        setTimeout(() => {
+            chrome.storage.session.get(["times"]).then(newStorage => {
+                if (newStorage.times) {
+                    chrome.scripting.executeScript({
+                        files: ["./scripts/automator.js"],
+                        target: { tabId: tab }
+                    });
+                }
+            });
+        }, interval * 1000);
     }
 });
 
--- scripts/automator.js
+++ scripts/automator.js
@@ -2,20 +2,15 @@ try {
     var textarea = document.getElementsByTagName("textarea")[0];
     var submit = document.querySelectorAll("[type=submit]")[0];
 
-    chrome.storage.session.get(["character", "interval", "times"]).then(storage => {
+    chrome.storage.session.get(["character", "times"]).then(storage => {
+        if (storage.times > 1) {
+            chrome.storage.session.set({ times: storage.times - 1 });
+        } else {
+            chrome.storage.session.clear();
+        }
+
         textarea.value += storage.character;
         submit.click();
-        setTimeout(() => {
-            chrome.storage.session.get(["times"]).then(newStorage => {
-                if (newStorage.times) {
-                    if (storage.times > 1) {
-                        chrome.storage.session.set({ times: storage.times - 1 });
-                    } else {
-                        chrome.storage.session.clear();
-                    }
-                }
-            });
-        }, storage.interval * 1000);
     });
 } catch (error) {
     console.log(error);
